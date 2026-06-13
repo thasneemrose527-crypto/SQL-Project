c9562771a5b70f9dcb86f8d5ce7a6de342f4554b
thasneem05 <thasneemrose527@gmail.com>
2026-06-13 08:13:51 +0530

    Add Employee Database SQL Project

---
+++ Employee_Database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Employees (
+    EmployeeID INT,
+    Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO Employees VALUES
+(1,'Ravi','HR',30000),
+(2,'Priya','IT',50000),
+(3,'Arun','Sales',40000);
+
+SELECT * FROM Employees;
